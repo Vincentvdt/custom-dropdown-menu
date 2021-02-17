1ce62558263d1f730891cd12d4ba0ed81975b71d
Vincent <51759317+Vincentvdt@users.noreply.github.com>
2021-02-17 18:13:02 +0100

    - Optimisation et factorisation du code. - Click Outside - Fonction disabled & reset - Changement non de classe HTML

--- main.js
+++ main.js
@@ -1,68 +1,116 @@
-let selectItems = document.querySelectorAll('.select-items');
+function showDropdown(btn, dropdown, arrows = null) {
+    if (!btn.classList.contains('disabled')) {
 
-selectItems.forEach((selects) => {
-    let select = selects.querySelector('.select');
+        dropdown.classList.toggle('is-visible');
+        btn.classList.toggle("actived");
 
-    let selectDropdown = selects.querySelector('.select-dropdown');
-
-
-    select.addEventListener('click', () => {
-        if (selects.classList.contains('disabled')) return;
-        selectDropdown.classList.toggle("is-visible");
-        select.classList.toggle("actived");
-
-        let options = selectDropdown.querySelectorAll('li');
-        let selectTitle = select.querySelector('.select-title');
-        let selectInput = select.querySelector('.select-input');
-        options.forEach((option) => {
-            option.addEventListener('click', () => {
-
-
-                selectTitle.dataset.value = option.dataset.value;
-                selectTitle.textContent = selectTitle.dataset.value;
-                selectTitle.classList.add('is-focus');
-                selectInput.value = option.dataset.value;
-
-                select.querySelector(".arrow-line.first").classList.add("cross");
-                select.querySelector(".arrow-line.second").classList.add("cross");
-                select.querySelector(".select-arrow").dataset.state = "cross";
-
-                if (selectDropdown.classList.contains("is-visible")) {
-                    selectDropdown.classList.toggle("is-visible");
-                    select.classList.toggle("actived");
-                }
-
-                let inputs = document.querySelectorAll('.select-input')
-                if (inputs[0].value !== "") {
-                    selectItems[1].classList.remove('disabled');
-                }
-                if (inputs[1].value !== "") {
-                    selectItems[2].classList.remove('disabled');
-                }
+        if (arrows !== null) {
+            arrows.forEach((arrow) => {
+                arrow.classList.toggle('actived');
             })
+        }
 
-        })
-
-        document.addEventListener('click', (e) => {
-
-            if (e.target !== selectDropdown && e.target.parentElement.parentElement !== selectDropdown && e.target !== selectTitle && e.target !== select && e.target !== select.querySelector('.select-arrow')) {
-                if (selectDropdown.classList.contains("is-visible")) {
-                    selectDropdown.classList.toggle("is-visible");
-                    select.classList.toggle("actived");
-                }
-            }
-        })
-
-
-    })
-    if (select.querySelector(".select-arrow").dataset.state === 'cross') {
-        select.querySelector(".select-arrow").addEventListener('click',()=>{
-            select.querySelector(".select-arrow").dataset.state = "arrow";
-
-        })
     }
-})
 
 
+}
 
 
+function disabled() {
+    const inputs = document.querySelectorAll('.select-input');
+    const btns = document.querySelectorAll('.select-btn');
+
+    for (let i = 0; i < 2; i++) {
+        if (inputs[i].value) {
+            btns[i + 1].classList.remove('disabled');
+        } else {
+            btns[i + 1].classList.add('disabled');
+        }
+    }
+}
+
+function reset() {
+    const btns = document.querySelectorAll('.select-btn');
+    btns.forEach((btn) => {
+        let btnText = btn.querySelector('.select-title');
+        let btnArrows = btn.querySelectorAll('.arrow-line');
+        let input = btn.querySelector('.select-input');
+
+
+        input.value = "";
+        btnText.textContent = btnText.dataset.value;
+        btnText.classList.remove('is-focus');
+
+
+        btnArrows.forEach((arrow) => {
+            arrow.classList.remove('is-focus');
+        })
+    })
+
+    disabled();
+}
+
+
+const resetBtn = document.querySelector('input[type=reset]');
+resetBtn.addEventListener('click', reset);
+
+const selects = document.querySelectorAll('.select');
+
+selects.forEach((select) => {
+    let selectBtn = select.querySelector('.select-btn');
+    let dropdown = select.querySelector('.select-dropdown');
+
+    const dropdownOptions = dropdown.querySelectorAll('.custom-options li');
+
+
+    selectBtn.addEventListener("click", () => {
+        showDropdown(selectBtn, dropdown, btnArrows);
+        if (dropdown.classList.contains('is-visible')) {
+            document.addEventListener("click", function _listener(e) {
+                if (e.target !== dropdown && e.target !== selectBtn && e.target.parentElement.className !== "custom-options") {
+
+                    showDropdown(selectBtn, dropdown, btnArrows);
+                }
+                document.removeEventListener("click", _listener, true);
+            }, true);
+        }
+    });
+
+    let btnText = select.querySelector('.select-title');
+    let btnInput = select.querySelector('.select-input');
+    let btnArrows = select.querySelectorAll('.arrow-line');
+
+    dropdownOptions.forEach((option) => {
+        option.addEventListener("click", () => {
+            let value = option.dataset.value
+
+            btnText.textContent = value;
+            btnInput.value = value;
+
+            if (!btnText.classList.contains('is-focus')) {
+                btnText.classList.toggle('is-focus');
+                btnArrows.forEach((arrow) => {
+                    arrow.classList.toggle('is-focus');
+                })
+            }
+
+            showDropdown(selectBtn, dropdown, btnArrows);
+            disabled();
+
+        });
+
+    });
+
+
+});
+/*
+function  outside() {
+
+    const btns = document.querySelectorAll('.select-btn')
+    btns.forEach((btn)=>{
+        btn.nextElementSibling.classList.remove('is-visible');
+    })
+}
+document.addEventListener('click', ()=>{
+    outside();
+})*/
